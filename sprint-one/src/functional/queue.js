a74b0a39c11ead921464102ab2a06d1a888b52ba
Eric Bayasgalan <ewiz1020@gmail.com>
2020-11-18 12:30:34 -0800

    Complete stack DS

--- sprint-one/src/functional/queue.js
+++ sprint-one/src/functional/queue.js
@@ -1,18 +1,32 @@
 var Queue = function() {
-  var someInstance = {};
+  var someInstance = {
+  };
 
   // Use an object with numeric keys to store values
-  var storage = {};
+  var storage = {
+    size: 0,
+    counter: 0
+  };
 
   // Implement the methods below
 
   someInstance.enqueue = function(value) {
+    var index = storage.size;
+    storage[index] = value;
+    storage.size++;
   };
 
   someInstance.dequeue = function() {
+    if (storage.size === 0) { return; }
+    var deleted = storage.counter;
+    delete storage[counter];
+    storage.counter++;
+    storage.size--;
+    return deleted;
   };
 
   someInstance.size = function() {
+    return storage.size;
   };
 
   return someInstance;
--- sprint-one/src/functional/stack.js
+++ sprint-one/src/functional/stack.js
@@ -2,16 +2,29 @@ var Stack = function() {
   var someInstance = {};
 
   // Use an object with numeric keys to store values
-  var storage = {};
+  var storage = {
+    size: 0
+  };
 
   // Implement the methods below
   someInstance.push = function(value) {
+    var index = storage.size;
+    storage[index] = value;
+    storage.size++;
   };
 
   someInstance.pop = function() {
+    var index = storage.size - 1;
+    var deleted = storage[index];
+    delete storage[index];
+    if (storage.size !== 0) {
+      storage.size--;
+    }
+    return deleted;
   };
 
   someInstance.size = function() {
+    return storage.size;
   };
 
   return someInstance;
