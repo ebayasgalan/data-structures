446a7153bfc6f8947eebf347eec204f794cd1cba
Eric Bayasgalan <ewiz1020@gmail.com>
2020-11-26 15:08:46 -0800

    Complete advanced content for HashTable DS

--- sprint-two/src/hashTable.js
+++ sprint-two/src/hashTable.js
@@ -1,46 +1,81 @@
-// pseudoclassical style
-
 var HashTable = function() {
   this._limit = 8;
   this._storage = LimitedArray(this._limit);
+  this._count = 0;
 };
-
-// O(1) time complexity: constant
+// O(1): constant time complexity
 HashTable.prototype.insert = function(k, v) {
   var index = getIndexBelowMaxForKey(k, this._limit);
-  if (this._storage.get(index) === undefined) {
-    this._storage.set(index, []);
-    this._storage.get(index).push([k, v]);
-  } else {
-    this._storage.get(index).forEach(bucket => {
-      if (bucket[0] === k) {
-        bucket[1] = v;
-      }
-    });
-    this._storage.get(index).push([k, v]);
+  var bucket = this._storage.get(index);
+  if (!bucket) {
+    bucket = [];
+    this._storage.set(index, bucket);
+  }
+  let found = false;
+  // iterate over to update if key exists
+  for (let i = 0; i < bucket.length; i++) {
+    var tuple = bucket[i];
+    if (tuple[0] === k) {
+      tuple[1] = v;
+      found = true;
+      break;
+    }
+  }
+  // add new tuple if no key is found and resize if needed
+  if (!found) {
+    bucket.push([k, v]);
+    this._count++;
+    if (this._count > this._limit * .75) {
+      this.resize(this._limit * 2);
+    }
   }
 };
-
-// O(n) time complexity: linear
+// O(n): linear time complexity
 HashTable.prototype.retrieve = function(k) {
   var index = getIndexBelowMaxForKey(k, this._limit);
-  var elem = this._storage.get(index);
-  var result;
-  elem.forEach(el => {
-    if (el[0] === k) {
-      result = el[1];
+  var bucket = this._storage.get(index);
+  if (!bucket) { return undefined; }
+  for (let i = 0; i < bucket.length; i++) {
+    var tuple = bucket[i];
+    if (tuple[0] === k) {
+      return tuple[1];
     }
-  });
-  return result;
-
+  }
+  return null;
 };
-
+// O(n): linear time complexity
 HashTable.prototype.remove = function(k) {
   var index = getIndexBelowMaxForKey(k, this._limit);
-  this._storage.get(index).forEach(elem => {
-    if (elem[0] === k) {
-      delete elem[1];
+  var bucket = this._storage.get(index);
+  if (!bucket) {
+    return null;
+  }
+  for (var i = 0; i < bucket.length; i++) {
+    var tuple = bucket[i];
+    if (tuple[0] === k) {
+      bucket.splice(i, 1);
+      this._count--;
+      if (this._count < this._limit * .25) {
+        this.resize(this._limit / 2);
+      }
+      return tuple[1];
     }
-  });
+  }
+  return null;
 };
 
+HashTable.prototype.resize = function(newLimit) {
+  var oldStorage = this._storage;
+
+  this._limit = newLimit;
+  this._storage = LimitedArray(newLimit);
+  this._count = 0;
+
+  oldStorage.each(function(bucket) {
+    if (!bucket) { return; }
+    for (let i = 0; i < bucket.length; i++) {
+      var tuple = bucket[i];
+      this.insert(tuple[0], tuple[1]);
+    }
+  }.bind(this));
+};
